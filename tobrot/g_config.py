f82914a270e400b0d7929962536448185c71212a
Jikku <66976170+jikkubot@users.noreply.github.com>
2021-02-09 01:01:07 +0400

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -1,13 +1,15 @@
 from tobrot.sample_config import Config
-#Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
-    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
-    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
-    #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
+    TG_BOT_TOKEN= "1288713654:AAHaJ7G2S0w3hmPrraKYBfTJexWJC47MXVs"
+    APP_ID = 1618055
+    API_HASH = "a4a50a6e5f9b8550884d61d7ee49e5f4"
+    OWNER_ID = 976661372
+    AUTH_CHANNEL = [-1001161510664]
+    DESTINATION_FOLDER = "fcgdrive" #Name of your folder read readme(not id of the folder)
+    RCLONE_CONFIG = ""
+    YTDL_COMMAND = "ytdl@JikkutorfileBot"
+    LEECH_COMMAND = "leech@JikkutorfileBot"
+    SAVE_THUMBNAIL = "savethumbnail@JikkutorfileBot"
+    CLEAR_THUMBNAIL = "clearthumbnail@JikkutorfileBot"
+    UPLOAD_AS_DOC = "True"
